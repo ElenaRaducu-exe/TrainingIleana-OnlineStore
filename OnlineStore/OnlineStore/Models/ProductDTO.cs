1997ab03fa94849f26fe41184d055147b9fddb93
Raducu Ileana <Elena.Raducu@exesoftware.ro>
2026-09-09 16:13:00 +0300

    feat: implement form validation

--- OnlineStore/OnlineStore/Models/ProductDTO.cs
+++ OnlineStore/OnlineStore/Models/ProductDTO.cs
@@ -1,15 +1,33 @@
-﻿namespace OnlineStore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineStore.Models
 {
     public class ProductDTO
     {
         public int Id { get; set; }
-        public string Name { get; set; } 
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
         public string? Description { get; set; }
+
+        [Required]
         public decimal Price { get; set; }
+
+        [Required]
         public int Stock { get; set; }
+
+        [Required]
         public string? ImageUrl { get; set; }
+
+        [Required]
         public bool IsActive { get; set; }
+
+        [Required]
         public string Category {  get; set; }
+
+        [Required]
         public string Brand {  get; set; }
     }
 }
