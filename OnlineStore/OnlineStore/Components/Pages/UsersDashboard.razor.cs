894d344a41b4626ce85f6fdd1858c0b07039fdb2
Raducu Ileana <Elena.Raducu@exesoftware.ro>
2026-09-09 16:52:36 +0300

    refactor: move component logic to code-behind file for better separation of concerns

--- OnlineStore/OnlineStore/Components/Pages/UsersDashboard.razor.cs
+++ OnlineStore/OnlineStore/Components/Pages/UsersDashboard.razor.cs
@@ -1,7 +1,52 @@
-﻿namespace OnlineStore.Components.Pages
+using Microsoft.AspNetCore.Components;
+using OnlineStore.DBModels;
+using OnlineStore.Services.Contracts;
+
+namespace OnlineStore.Components.Pages
 {
-    public class UserDashboard
+    public partial class UsersDashboard : ComponentBase
     {
-        //blazor code behind file
+        [Inject]
+        private IHttpClientFactory _httpClientFactory { get; set; }
+
+        [Inject]
+        private NavigationManager _navigation { get; set; }
+
+        public List<User> UsersList = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            httpClient.BaseAddress = new Uri(_navigation.BaseUri);
+
+            UsersList = await httpClient.GetFromJsonAsync<List<User>>("api/users/dashboard");
+        }
+
+        private async Task UpdateActiveMode(int id)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            httpClient.BaseAddress = new Uri(_navigation.BaseUri);
+
+            var user = await httpClient.GetFromJsonAsync<User>($"api/users/dashboard/{id}");
+
+            var response = await httpClient.PutAsJsonAsync($"api/users/dashboard/update/active/{id}", user);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var updatedUser = await response.Content.ReadFromJsonAsync<User>();
+
+                if (updatedUser != null)
+                {
+                    var userList = UsersList.FirstOrDefault(u => u.Id == id);
+
+                    if (userList != null)
+                    {
+                        userList.IsActive = updatedUser.IsActive;
+                    }
+
+                    StateHasChanged();
+                }
+            }
+        }
     }
 }
